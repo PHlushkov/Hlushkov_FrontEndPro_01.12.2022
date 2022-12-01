00b3ebd0e2d9db373a32bc0effe3dcdd96034f74
Pavel Glushkov <pavelgluskov264@gmail.com>
2022-12-02 01:31:00 +0200

    add hw4

---
+++ HW4/index.js
@@ -0,0 +1,5 @@
+let firstNumber = +prompt("Enter first number");
+let secondNumber = +prompt("Enter second number");
+let thirdNumber = +prompt("Enter third number");
+
+alert((firstNumber + secondNumber + thirdNumber) / 3);
