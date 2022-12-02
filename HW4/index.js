9de666826fce3c5bf3846607afabb55a0e9f983e
Pavel Glushkov <pavelgluskov264@gmail.com>
2022-12-02 14:30:10 +0200

    change from let to const

---
+++ HW4/index.js
@@ -0,0 +1,5 @@
+const firstNumber = +prompt("Enter first number");
+const secondNumber = +prompt("Enter second number");
+const thirdNumber = +prompt("Enter third number");
+
+alert((firstNumber + secondNumber + thirdNumber) / 3);
