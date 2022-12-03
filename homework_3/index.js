eaddbcd19d60225e9dde8592ec85ec8103f6b2a1
Pavel Glushkov <pavelgluskov264@gmail.com>
2022-12-03 21:46:00 +0200

    add homework5

--- homework_3/index.js
+++
@@ -1,7 +0,0 @@
-let firstValue = prompt("First Number")
-let secondValue = prompt("Second Number")
-
-alert(+firstValue + +secondValue)
-alert(firstValue - secondValue)
-alert(firstValue * secondValue)
-alert(firstValue / secondValue)
