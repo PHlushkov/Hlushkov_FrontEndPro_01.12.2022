bcc34035a2883fc6928643e0dc6fd0696c6baaf9
Pavel Glushkov <pavelgluskov264@gmail.com>
2022-12-12 18:35:06 +0200

    Error_correction

--- homework10/index.js
+++ homework10/index.js
@@ -1,10 +1,15 @@
-const enterNumber = prompt("Укажи значение через ',' (Пример: 1, 2, 3, JS)");
+const enterNumber = +prompt("Вкажи довжину массиву");
 
-const arr = enterNumber.split(",");
+const arr = [];
+console.log(arr);
+
+for (let i = 0; i < enterNumber; i++) {
+  arr.push(prompt(`Значення массиву ${i}`));
+}
 console.log(arr);
 
 arr.sort();
 console.log(arr);
 
-arr.splice(2, 3);
+arr.splice(1, 3);
 console.log(arr);
