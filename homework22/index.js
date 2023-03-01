d194a67120c25ac61ed702d67756d1b7e31c2f18
Pavel Glushkov <pavelgluskov264@gmail.com>
2023-03-01 16:07:06 +0200

    add homework22

---
+++ homework22/index.js
@@ -0,0 +1,88 @@
+"use strict";
+
+const tsetInfo = addUserElem.addEventListener("click", () => {
+  infoUserElem.style.display = "none";
+  showForm();
+});
+
+for (const key in localStorage) {
+  if (Object.hasOwnProperty.call(localStorage, key)) {
+    const userInfo = createElem(
+      "li",
+      { className: "user", id: key },
+      null,
+      key,
+      listUsersElem
+    );
+    const blockElem = createElem(
+      "div",
+      { className: "btn-control" },
+      null,
+      null,
+      userInfo
+    );
+    createElem(
+      "button",
+      null,
+      {
+        click: () => {
+          const idUser = userInfo.getAttribute("id");
+          const infoLocalStorage = JSON.parse(localStorage.getItem(idUser));
+          infoUserElem.innerHTML = `<h2>Details user ${idUser} :  <h2>`;
+          const detailsInfo = createElem(
+            "ul",
+            { className: "details-user" },
+            null,
+            null,
+            infoUserElem
+          );
+          infoLocalStorage.forEach((element) => {
+            createElem("li", null, null, element, detailsInfo);
+          });
+          createElem(
+            "button",
+            { className: "back-btn" },
+            {
+              click: () => {
+                document.location.reload();
+              },
+            },
+            "Повернутися до списку користувачів",
+            infoUserElem
+          );
+        },
+      },
+      "View",
+      blockElem
+    );
+    createElem(
+      "button",
+      null,
+      {
+        click: () => {
+          const idUser = userInfo.getAttribute("id");
+          infoUserElem.style.display = "none";
+          showForm(idUser);
+        },
+      },
+      "Edit",
+      blockElem
+    );
+    createElem(
+      "button",
+      null,
+      {
+        click: () => {
+          const idUser = userInfo.getAttribute("id");
+          const confirmation = confirm(`Видалити користувача ${idUser}?`);
+          if (confirmation === true) {
+            localStorage.removeItem(idUser);
+            document.location.reload();
+          }
+        },
+      },
+      "Remove",
+      blockElem
+    );
+  }
+}
