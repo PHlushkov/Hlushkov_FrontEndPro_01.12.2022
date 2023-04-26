abe3de2a88e3e84fc41a46e4f6c20374c953c3d4
Pavel Glushkov <pavelgluskov264@gmail.com>
2023-04-26 23:34:32 +0300

    add homework28

---
+++ react/my-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Calculator from "./components/Calculator";
+
+function App() {
+  return (
+    <div className="container">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
